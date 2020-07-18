6fe45b0227cbc679395be8e15b1b39e4dfc7ace4
SrLozano <100383511@alumnos.uc3m.es>
2020-07-18 11:52:27 +0200

    Timeline added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeftTabComponent } from './left-tab/left-tab.component';
 import { RightTabComponent } from './right-tab/right-tab.component';
 import {MatButtonModule} from '@angular/material/button';
-import { TabHeaderComponent } from './tab-header/tab-header.component'; 
+import { TabHeaderComponent } from './tab-header/tab-header.component';
+import { TimelineComponent } from './timeline/timeline.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftTabComponent,
     RightTabComponent,
-    TabHeaderComponent
+    TabHeaderComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
