746ad1671d7c9367d17aea0c99e7aa1515ce1356
SrLozano <100383511@alumnos.uc3m.es>
2020-08-13 12:30:27 +0200

    ✨ Responsive settings tab and button for going to settings

--- src/app/speed-dial-fab/speed-dial-fab.component.ts
+++ src/app/speed-dial-fab/speed-dial-fab.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { speedDialFabAnimations } from './speed-dial-fab.animations';
+import {LeftTabComponent} from '../left-tab/left-tab.component'
 
 @Component({
   selector: 'app-speed-dial-fab',
   templateUrl: './speed-dial-fab.component.html',
   styleUrls: ['./speed-dial-fab.component.scss'],
+  providers: [LeftTabComponent],
   animations: speedDialFabAnimations
 })
 export class SpeedDialFabComponent implements OnInit {
@@ -16,13 +18,17 @@ export class SpeedDialFabComponent implements OnInit {
 
   public fabTogglerState = 'inactive';
 
-  constructor() { }
+  constructor(public leftTab:LeftTabComponent) { }
 
   public ngOnInit() {
     const maxButtons = 7;
     if (this.options.buttons.length > maxButtons) {
       this.options.buttons.splice(5, this.options.buttons.length - maxButtons);
     }
+  }
+
+  public open(){
+    this.leftTab.openNav();
   }
 
   public showItems() {
